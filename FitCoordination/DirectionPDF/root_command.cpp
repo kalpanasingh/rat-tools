d14d67e984d072cacba6b5aea9ce52e4b02a45a1
Ian Coulter (SNO+ 2009) <coulter@pplxint5.physics.ox.ac.uk>
2012-06-07 14:29:34 +0100

    Make changes to co-ordinator as suggested to work with commands to choose directory, scintillator, etc...

--- FitCoordination/DirectionPDF/root_command.cpp
+++ FitCoordination/DirectionPDF/root_command.cpp
@@ -1,5 +1,5 @@
 {
-  gROOT->ProcessLine(".L /home/coulter/rat-tools/FitCoordination/DirectionPDF/direction_pdf.cpp+");
-  PlotDirections("/home/coulter/rat-tools/FitCoordination/DirectionPDF/PDF_10MeV_5k.root");
+  gROOT->ProcessLine(".L direction_pdf.cpp+");
+  PlotDirections("PDF_10MeV_5k.root");
   gROOT->ProcessLine(".q");
 }
