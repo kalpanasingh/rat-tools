08500e76016c4fe5b8ed1900b2dd7f81b3a11cc8
Phil Jones <p.g.jones@qmul.ac.uk>
2012-06-20 20:55:01 +0100

    Bug fixes and general tidy up of the EffectiveTransit coordinator.

--- FitCoordination/EffectiveTransit/ProduceData.py
+++ FitCoordination/EffectiveTransit/ProduceData.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python
-import os, sys, string, EffectiveTransitTimeUtil
+# Run rat and produce data for each position in the posSet
 # Author P G Jones - 13/03/2011 <p.jones22@physics.ox.ac.uk>
+import os
+import sys
+import string
+import EffectiveTransitTimeUtil
 
 def ProduceRunMacFile( options ):
-        """Produces and then runs the appropriate mac files."""
-        inFile = open( "Base.mac", "r" )
-        rawText = string.Template( inFile.read() )
-        inFile.close()
-        for pos in EffectiveTransitTimeUtil.posSet:
-                outText = rawText.substitute( SourcePos = str( "%.0f" % pos ),
-                                              GeoFile = options.geoFile,
-                                              ScintMaterial = options.scintMaterial,
-                                              Particle = options.particle )
+    """Produces and then runs the appropriate mac files."""
+    inFile = open( "Base.mac", "r" )
+    rawText = string.Template( inFile.read() )
+    inFile.close()
+    for pos in EffectiveTransitTimeUtil.posSet:
+        outText = rawText.substitute( SourcePos = str( "%.0f" % pos ),
+                                      GeoFile = options.geoFile,
+                                      ScintMaterial = options.scintMaterial,
+                                      Particle = options.particle )
         outFileName = "P%.0f.mac" % pos
         outFile = open( outFileName, "w" )
         outFile.write( outText )
         outFile.close()
         os.system( "rat " + outFileName )
         os.remove( outFileName )
-        
+    
 import optparse
 if __name__ == '__main__':
-        parser = optparse.OptionParser( usage = "usage: %prog [options] target", version="%prog 1.0" )
-        parser.add_option( "-g", type="string", dest="geoFile", help="Geometry File to use, location must be absolute or relative to target.", default="geo/snoplus.geo" )
-        parser.add_option( "-s", type="string", dest="scintMaterial", help="Scintillator material.", default="labppo_scintillator" )
-        parser.add_option( "-p", type="string", dest="particle", help="Particle type.", default="e-" )
-        (options, args) = parser.parse_args()
-        ProduceRunMacFile( options )
-        
-                        
+    parser = optparse.OptionParser( usage = "usage: %prog [options] target", version="%prog 1.0" )
+    parser.add_option( "-g", type="string", dest="geoFile", help="Geometry File to use, location must be absolute or relative to target.", default="geo/snoplus.geo" )
+    parser.add_option( "-s", type="string", dest="scintMaterial", help="Scintillator material.", default="labppo_scintillator" )
+    parser.add_option( "-p", type="string", dest="particle", help="Particle type.", default="e-" )
+    (options, args) = parser.parse_args()
+    ProduceRunMacFile( options )
+    
+            
