c3e9fd1014af93f2962e6ee00f200a6aea67cce6
P G Jones <p.g.jones@qmul.ac.uk>
2012-08-17 15:57:56 +0100

    Fix indentation in Energy coordinator.

--- FitCoordination/EnergyLookup/DiagnosticPlotFiles.py
+++ FitCoordination/EnergyLookup/DiagnosticPlotFiles.py
@@ -5,51 +5,51 @@ import ROOT, rat, EnergyLookupUtil
 
 def PlotNHitPerPos():
     """ Plot the Nhit per radial position for different energies."""
-	c1 = ROOT.TCanvas()
-	l1 = ROOT.TLegend( 0.1, 0.7, 0.3, 0.9)
-	nhitPerMeVtable = EnergyLookupUtil.NhitPerMeVEnergyPos()
-	graphs = []
-	for iPad, energy in enumerate( EnergyLookupUtil.EnergySet ):
-		graph = ROOT.TGraph()
-		for graphPoint in range( 0, len( EnergyLookupUtil.PosSet ) ):
-			graph.SetPoint( graphPoint, EnergyLookupUtil.PosSet[graphPoint], nhitPerMeVtable[iPad][graphPoint] )
-		graph.SetMarkerColor( iPad + 2 )
-		graphs.append( graph )
-		l1.AddEntry( graph, str(energy), "P" )
+    c1 = ROOT.TCanvas()
+    l1 = ROOT.TLegend( 0.1, 0.7, 0.3, 0.9)
+    nhitPerMeVtable = EnergyLookupUtil.NhitPerMeVEnergyPos()
+    graphs = []
+    for iPad, energy in enumerate( EnergyLookupUtil.EnergySet ):
+        graph = ROOT.TGraph()
+        for graphPoint in range( 0, len( EnergyLookupUtil.PosSet ) ):
+            graph.SetPoint( graphPoint, EnergyLookupUtil.PosSet[graphPoint], nhitPerMeVtable[iPad][graphPoint] )
+        graph.SetMarkerColor( iPad + 2 )
+        graphs.append( graph )
+        l1.AddEntry( graph, str(energy), "P" )
 
-	for iGraph, graph in enumerate( graphs ):
-		if iGraph == 0:
-			graph.Draw( "A*" )
-			graph.GetYaxis().SetRangeUser( 0, 2000 )
-		else:
-			graph.Draw( "*" )
-	l1.Draw()
+    for iGraph, graph in enumerate( graphs ):
+        if iGraph == 0:
+            graph.Draw( "A*" )
+            graph.GetYaxis().SetRangeUser( 0, 2000 )
+        else:
+            graph.Draw( "*" )
+    l1.Draw()
 
-	raw_input( "Hit Enter to exit." )
+    raw_input( "Hit Enter to exit." )
 
 def PlotNHitPerMeV():
     """ Plot the Nhit per energy for different radial positions."""
-	l1 = ROOT.TLegend( 0.1, 0.7, 0.3, 0.9)
-	nhitPerMeVtable = EnergyLookupUtil.NhitPerMeVEnergyPos()
-	graphs = []
-	graphPoints = []
-	for iPos in range( 0, len( EnergyLookupUtil.PosSet ) ):
-		graphs.append( ROOT.TGraph() )
-		graphPoints.append(0)
-		for iEnergy, energy in enumerate( EnergyLookupUtil.EnergySet ):
-			graphs[iPos].SetPoint( graphPoints[iPos], energy, nhitPerMeVtable[iEnergy][iPos] )
-			graphPoints[iPos] += 1
-	c1 = ROOT.TCanvas()
-	c1.Divide( 2, len( EnergyLookupUtil.PosSet ) / 2 )
-	masterFit = ROOT.TF1( "master", "[0] + [1]*x", 0, 5.5 )
-	fits = []
-	for iPad, graph in enumerate( graphs ):
-		c1.cd( iPad + 1 )
-		graph.Draw("A*")
-		fits.append( masterFit.Clone( "new" + str(iPad) ) )
-		graph.Fit( fits[iPad], "r" )
+    l1 = ROOT.TLegend( 0.1, 0.7, 0.3, 0.9)
+    nhitPerMeVtable = EnergyLookupUtil.NhitPerMeVEnergyPos()
+    graphs = []
+    graphPoints = []
+    for iPos in range( 0, len( EnergyLookupUtil.PosSet ) ):
+        graphs.append( ROOT.TGraph() )
+        graphPoints.append(0)
+        for iEnergy, energy in enumerate( EnergyLookupUtil.EnergySet ):
+            graphs[iPos].SetPoint( graphPoints[iPos], energy, nhitPerMeVtable[iEnergy][iPos] )
+            graphPoints[iPos] += 1
+    c1 = ROOT.TCanvas()
+    c1.Divide( 2, len( EnergyLookupUtil.PosSet ) / 2 )
+    masterFit = ROOT.TF1( "master", "[0] + [1]*x", 0, 5.5 )
+    fits = []
+    for iPad, graph in enumerate( graphs ):
+        c1.cd( iPad + 1 )
+        graph.Draw("A*")
+        fits.append( masterFit.Clone( "new" + str(iPad) ) )
+        graph.Fit( fits[iPad], "r" )
 
-	raw_input( "Hit Enter to exit." )
+    raw_input( "Hit Enter to exit." )
 
 ROOT.gROOT.SetStyle("Plain")
 ROOT.gStyle.SetCanvasBorderMode(0)
@@ -68,4 +68,4 @@ if __name__ == '__main__':
         PlotNHitPerMeV()
     else:
         PlotNHitPerPos()
-		
+        
