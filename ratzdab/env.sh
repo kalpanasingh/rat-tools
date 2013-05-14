e5be05000ece017c7e411a6b372a6bc793c7a947
Richie Bonventre <rbonv@hep.upenn.edu>
2013-05-14 15:44:14 -0400

    fixed not being able to source ratzdab env.sh from anywhere

--- ratzdab/env.sh
+++ ratzdab/env.sh
@@ -1,6 +1,14 @@
-export LD_LIBRARY_PATH=$PWD/lib:$PWD/contrib/disp/lib:$LD_LIBRARY_PATH
-export DYLD_LIBRARY_PATH=$PWD/lib:$PWD/contrib/disp/lib:$DYLD_LIBRARY_PATH
-export RATZDAB_ROOT=$PWD
-export PYTHONPATH=$PWD/python:$PYTHONPATH
-export PATH=$PWD/bin:$PATH
+#!/bin/bash 
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+export LD_LIBRARY_PATH=$DIR/lib:$PWD/contrib/disp/lib:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$DIR/lib:$PWD/contrib/disp/lib:$DYLD_LIBRARY_PATH
+export RATZDAB_ROOT=$DIR
+export PYTHONPATH=$DIR/python:$PYTHONPATH
+export PATH=$DIR/bin:$PATH
 
