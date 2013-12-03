c1422e426780d7f09aa0f828264085e96f21039c
pgjones <p.g.jones@qmul.ac.uk>
2013-12-03 15:28:31 +0000

    New GeoTools, used to calculate geomtric positions.
    See SNO+-doc-2222 and documentation within the files.

---
+++ GeoTools/belly_ropes_db.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+#
+# belly_ropes_db.py
+#
+# Outputs Belly plate positions and rool from the approximate positions in 17-702-F-6499-02_Rev_01.
+# Output is in ratdb format.
+#
+# Author P G Jones - 21/06/2013 <p.g.jones@qmul.ac.uk> : First revision
+####################################################################################################
+import math
+import yaml
+
+initial_offset_to_x = 18.0 + 0.0#1.55
+separation = 36
+rotational_positions = [initial_offset_to_x + angle for angle in range(0, 360, separation)] 
+print rotational_positions
+rope_radial_pos = 6040.0 # RAT
+#rope_radial_pos = 6030.0 # SNOMAN
+
+db = { "x" : [], "y" : [], "z" : [], "roll" : [] }
+
+for rotation in rotational_positions:
+    z = 0.0 # Centre is that of the torus bit, which is at z=0
+    x = math.sin(math.radians(rotation)) * rope_radial_pos
+    y = math.cos(math.radians(rotation)) * rope_radial_pos
+    roll = rotation
+    db["x"].append(float("%.2f" % x))
+    db["y"].append(float("%.2f" % y))
+    db["z"].append(float("%.2f" % z))
+    db["roll"].append(float("%.2f" % (90.0+roll)))
+
+print "Ropes:"
+print  yaml.dump(db).replace("]", "],")
+
+db = {}
+plate_radial_pos = 6032.5
+db = { "x" : [], "y" : [], "z" : [], "roll" : [] }
+for rotation in rotational_positions:
+    z = 0.0 
+    x = math.sin(math.radians(rotation)) * plate_radial_pos
+    y = math.cos(math.radians(rotation)) * plate_radial_pos
+    roll = rotation + 180.0
+    db["x"].append(float("%.2f" % x))
+    db["y"].append(float("%.2f" % y))
+    db["z"].append(float("%.2f" % z))
+    db["roll"].append(float("%.2f" % ((90.0 + roll))))
+
+print "Plates:"
+print  yaml.dump(db).replace("]", "],")
