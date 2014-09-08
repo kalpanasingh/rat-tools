595f81958ab2f7450809a43d1c3e2149e0195815
Krishanu Majumdar <majumdar@pplxint8.physics.ox.ac.uk>
2014-09-08 15:37:16 +0100

    Updated AlphaUnseeded coordinator

--- FitCoordination/AlphaUnseeded/AnalyseData.py
+++ FitCoordination/AlphaUnseeded/AnalyseData.py
@@ -1,34 +1,78 @@
 #!usr/bin/env python
-import string, UnseededUtilities
+import string, Utilities, os
 # Author K Majumdar - 11/05/2012 <Krishanu.Majumdar@physics.ox.ac.uk>
+#        K Majumdar - 08/09/2014 - Cleanup of Coordinators for new DS
 
 
 def AnalyseRootFiles(options):
-	# returns the tail fraction value optimised for minimum overlap between alpha and electron tail/total PMTs ratio distributions
-	minlap = 10**9
-	minfrac = 1000.0
 
-	for tailfrac in UnseededUtilities.TailFracs:
-		Histogram1 = UnseededUtilities.ProduceRatioHistogram(UnseededUtilities.Particles[0] + ".root", tailfrac)
-		Histogram2 = UnseededUtilities.ProduceRatioHistogram(UnseededUtilities.Particles[1] + ".root", tailfrac)
-		overlap = UnseededUtilities.BhattacharyaCoeff(Histogram1, Histogram2)
+    # Load any parameters for running the macros on a Batch system
+    batch_params = None
+    if options.batch:
+        batch_params = {}
+    execfile(options.batch, {}, batch_params)
+	
+    # Load the batch submission script template
+    inFile = open("Template_Batch.sh", "r")
+    rawText = string.Template(inFile.read())
+    inFile.close()
+		
+    # Run the analysis on a Batch system
+    if options.batch:
+        outText = rawText.substitute(Preamble = "\n".join(s for s in batch_params['preamble']),
+                                     Ratenv = batch_params['ratenv'],
+                                     Cwd = os.environ['PWD'].replace("/.", "/"),
+                                     RunCommand = "python -c 'import AnalyseData; AnalyseData.AnalysisFunction(\"" + options.index + "\")'")
+        outFile = open("AnalyseData.sh", "w")
+        outFile.write(outText)
+        outFile.close()
+		
+        os.system(batch_params["submit"] + " AnalyseData.sh")
+		
+    # Else run the macro locally on an interactive machine				
+    else:
+        os.system("python -c 'import AnalyseData; AnalyseData.AnalysisFunction(\"" + options.index + "\")'")
+		
 
-		del Histogram1
-		del Histogram2
+# returns the tail fraction value, optimised for minimum overlap between alpha and electron tail/total PMTs ratio distributions
+def AnalysisFunction(options):
+	
+    minlap = 10**9
+    minFraction = 1000.0
 
-		if overlap < minlap:
-			minlap = overlap
-			minfrac = tailfrac
+    for tailFraction in Utilities.TailFracs:
+	
+        Histogram1 = Utilities.ProduceRatioHistogram(Utilities.Particles[0] + ".root", tailFraction)
+        Histogram2 = Utilities.ProduceRatioHistogram(Utilities.Particles[1] + ".root", tailFraction)
+        overlap = Utilities.BhattacharyaCoeff(Histogram1, Histogram2)
 
-	print "\n"
-	print "Please place the text below into the database file: CLASSIFIER_ALPHA_UNSEEDED.ratdb located in rat/data, replacing the existing corresponding text."
-	print "tail_fraction: %.2fd," % minfrac
-	print "\n"
+        del Histogram1
+        del Histogram2
+
+        if overlap < minlap:
+            minlap = overlap
+            minFraction = tailFraction
+
+    print "\n"
+    print "Please place the text below into the database file: CLASSIFIER_ALPHA_UNSEEDED.ratdb located in rat/data, replacing any existing entry with the same index."
+    print "\n"
+    print "{"
+    print "name: \"CLASSIFIER_ALPHA_UNSEEDED\","
+    print "index: \"" + index + "\","
+    print "valid_begin: [0, 0],"
+    print "valid_end: [0, 0],"
+    print "\n",
+    print "tail_fraction: " + str(minFraction) + "d,"
+    print "pmt_Min: " + str(Utilities.minTimeResid) + "d,"
+    print "pmt_Max: " + str(Utilities.maxTimeResid) + "d,"
+    print "}"
+    print "\n"
 
 
 import optparse
 if __name__ == '__main__':
-	parser = optparse.OptionParser(usage = "usage: %prog [options] target", version = "%prog 1.0")
-	parser.add_option("-i", type = "string", dest = "index", help = "RATDB index to place result.", default = "")
-	(options, args) = parser.parse_args()
-	AnalyseRootFiles(options)
+    parser = optparse.OptionParser(usage = "usage: %prog [options] target", version = "%prog 1.0")
+    parser.add_option("-b", type = "string", dest = "batch", help = "Run the macros in Batch mode")
+    parser.add_option("-i", type = "string", dest = "index", help = "RATDB index to place result.", default = "")
+    (options, args) = parser.parse_args()
+    AnalyseRootFiles(options)
--- FitCoordination/AlphaUnseeded/ProduceData.py
+++ FitCoordination/AlphaUnseeded/ProduceData.py
@@ -1,80 +1,80 @@
 #!/usr/bin/env python
-import os, sys, string, UnseededUtilities
+import os, sys, string, Utilities
 # Author K Majumdar - 10/05/2012 <Krishanu.Majumdar@physics.ox.ac.uk>
+#        K Majumdar - 08/09/2014 - Cleanup of Coordinators for new DS
 
 
-def ProduceRunMacFiles(options):
-    # produces and then runs the required mac files
+def ProduceRunMacros(options):
     
-    # load the basic mac file
-    inFile = open("Base.mac", "r")
-    rawText = string.Template(inFile.read())
-    inFile.close()
-
-    # load any batch options
+    # Load any parameters for running the macros on a Batch system
     batch_params = None
     if options.batch:
         batch_params = {}
-        execfile( options.batch, {}, batch_params )
+    execfile(options.batch, {}, batch_params)
 
-    inFile = open( "batch.sh", "r" )
-    rawScriptText = string.Template( inFile.read() )
-    inFile.close()
-
+    # Load any extra .ratdb files
     extraDB = ""
     if options.loadDB:
         extraDB = "/rat/db/load " + options.loadDB
+
+    # Load the basic macro template
+    inFile1 = open("Template_Macro.mac", "r")
+    rawText1 = string.Template(inFile1.read())
+    inFile1.close()
+	
+    # Load the batch submission script template
+    inFile2 = open("Template_Batch.sh", "r")
+    rawText2 = string.Template(inFile2.read())
+    inFile2.close()
     
-    # generate the specific mac files for each particle, with energies and corresponding names given in the Utilities tables
-    for index, energy in enumerate(UnseededUtilities.Energies):
+    # Generate the specific macro for each particle, with energies and corresponding names given in the Utilities tables
+    for index, energy in enumerate(Utilities.Energies):
         if index == 0:
-            generator = "/generator/vtx/set alpha 0 0 0 " + str(energy)
-            outfile = UnseededUtilities.Particles[0]
+            vertex = "/generator/vtx/set alpha 0 0 0 " + str(energy)
+            outfileName = Utilities.Particles[0]
             hadrons = ""
         elif index == 1:
-            generator = "/generator/vtx/set e- 0 0 0 " + str(energy)
-            outfile = UnseededUtilities.Particles[1]
+            vertex = "/generator/vtx/set e- 0 0 0 " + str(energy)
+            outfileName = Utilities.Particles[1]
             hadrons = "/rat/physics_list/OmitHadronicProcesses true"
 
-        outText = rawText.substitute(Generator = generator,
-                                     GeoFile = options.geoFile,
-                                     ScintMaterial = options.scintMaterial,
-                                     OmitHadrons = hadrons,
-                                     OutFileName = outfile + ".root",
-                                     ExtraDB = extraDB)
+        outText1 = rawText1.substitute(Hadrons = hadrons,
+                                       ExtraDB = extraDB,
+                                       GeoFile = options.geoFile,
+                                       ScintMaterial = options.scintMaterial,
+                                       FileName = outfileName + ".root",
+                                       Vertex = vertex)
+        outFile1 = open(outfileName + ".mac", "w")
+        outFile1.write(outText1)
+        outFile1.close()
 
-        outFile = open(outfile + ".mac", "w")
-        outFile.write(outText)
-        outFile.close()
-
-        print "Running " + outfile + ".mac and generating " + outfile + ".root"
-
-        if options.batch:
-            # run the macro on a batch system
-            outText = rawScriptText.substitute( Preamble = "\n".join(s for s in batch_params['preamble']),
-                                                    Cwd = os.environ['PWD'].replace("/.", "/"),
-                                                    Macro = outfile + ".mac",
-                                                    Ratenv = batch_params['ratenv'] )
-            outFile = open(outfile + ".sh", "w")
-            outFile.write(outText)
-            outFile.close()
-            os.system( batch_params["submit"] + " " + outfile +".sh" )
-
-        else:
-            # run the mac file for the particle
-            os.system("rat " + outfile + ".mac")
+        print "Running " + outfileName + ".mac and generating " + outfileName + ".root"
         
-            # delete the particle-specific mac file when running is complete
-            os.remove(outfile + ".mac")
+        # Run the macro on a Batch system
+        if options.batch:
+            outText2 = rawText2.substitute(Preamble = "\n".join(s for s in batch_params['preamble']),
+                                           Ratenv = batch_params['ratenv'],
+                                           Cwd = os.environ['PWD'].replace("/.", "/"),
+                                           RunCommand = "rat " + outfileName + ".mac")
+            outFile2 = open(outfileName + ".sh", "w")
+            outFile2.write(outText2)
+            outFile2.close()
+			
+            os.system( batch_params["submit"] + " " + outfileName +".sh" )        
+				
+        # Else run the macro locally on an interactive machine				
+        else:
+            os.system("rat " + outfileName + ".mac")
+            # delete the macro when running is complete
+            os.remove(outfileName + ".mac")
 
 
 import optparse
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage = "usage: %prog [options] target", version = "%prog 1.0")
-    parser.add_option("-g", type = "string", dest = "geoFile", help = "Geometry File to use - location must be absolute or relative to target.", default = "geo/snoplus.geo")
-    parser.add_option("-s", type = "string", dest = "scintMaterial", help = "Scintillator material.", default = "labppo_scintillator")
-    parser.add_option("-p", type = "string", dest = "particle", help = "Particle type - this is not relevant for this coordinator.", default = "")
-    parser.add_option("-b", type="string", dest="batch", help="Run in batch mode" )
-    parser.add_option("-l", type="string", dest="loaddb", help="Load an extra DB directory")
+    parser.add_option("-b", type = "string", dest = "batch", help = "Run the macros in Batch mode")
+    parser.add_option("-g", type = "string", dest = "geoFile", help = "Geometry File to use - location relative to rat/data/, default = geo/snoplus.geo", default = "geo/snoplus.geo")
+    parser.add_option("-l", type = "string", dest = "loadDB", help = "Load an extra .ratdb directory")
+    parser.add_option("-s", type = "string", dest = "scintMaterial", help = "Scintillator Material to use, default = labppo_scintillator", default = "labppo_scintillator")
     (options, args) = parser.parse_args()
-    ProduceRunMacFiles(options)
+    ProduceRunMacros(options)
