726c989c2ce0dcf717e2510ec1ca3fa2fde8a0d9
Krishanu Majumdar <majumdar@pplxint8.physics.ox.ac.uk>
2014-09-09 15:58:42 +0100

    Updated EnergyLookup coordinator

--- FitCoordination/EnergyLookup/ProduceData.py
+++ FitCoordination/EnergyLookup/ProduceData.py
@@ -1,86 +1,87 @@
 #!/usr/bin/env python
-import os
-import sys
-import string
-import EnergyLookupUtil
-# Author P G Jones - 12/09/2011 <p.jones22@physics.ox.ac.uk>
+import os, sys, string, Utilities
+# Author P G Jones - 12/09/2011 <p.g.jones@qmul.ac.uk>
+#        K Majumdar - 08/09/2014 - Cleanup of Coordinators for new DS
 
-def ProduceRunFiles(options):
-    # Load the raw Base file
-    inFile = open("Base.mac", "r")
-    rawText = string.Template(inFile.read())
-    inFile.close()
 
-    # load any batch options
+def ProduceRunMacros(options):
+    
+    # Load any parameters for running the macros on a Batch system
     batch_params = None
     if options.batch:
         batch_params = {}
-        execfile( options.batch, {}, batch_params )
+    execfile(options.batch, {}, batch_params)
 
-    inFile = open( "batch.sh", "r" )
-    rawScriptText = string.Template( inFile.read() )
-    inFile.close()
-
+    # Load any extra .ratdb files
     extraDB = ""
     if options.loadDB:
         extraDB = "/rat/db/load " + options.loadDB
+
+    # Load the basic macro template
+    inFile1 = open("Template_Macro.mac", "r")
+    rawText1 = string.Template(inFile1.read())
+    inFile1.close()
+	
+    # Load the batch submission script template
+    inFile2 = open("Template_Batch.sh", "r")
+    rawText2 = string.Template(inFile2.read())
+    inFile2.close()
     
-    # Setup arrays for positions and energies
-    if options.energies:
-        EnergyLookupUtil.SetEnergies(options.energies)
-    if options.positions:
-        EnergyLookupUtil.SetPositions(options.positions)
-    EnergyLookupUtil.SetMaterial(options.scintMaterial)
-
-    for pos in EnergyLookupUtil.PosSet:
-        for energy in EnergyLookupUtil.EnergySet:
-            # Set the generator text first
+    # Select which energies to use based on the material in the detector
+    energies = []
+    if options.scintMaterial == "lightwater_sno":
+        energies = Utilities.WaterEnergies
+    else:
+        energies = Utilities.ScintEnergies
+		
+    # Generate the specific macro for each energy/position combination, with energies and positions given in the Utilities tables
+    for position in Utilities.Positions:
+	
+        for energy in energies:
+		
             generator = "/generator/add combo gun:point\n" + \
-                "/generator/vtx/set " + options.particle + " 0 0 0 " + str(energy) + "\n" + \
-                "/generator/pos/set 0 %.0f 0" % pos 
-            # Create the correct file
-            outFileName = EnergyLookupUtil.GetFileName(pos, energy)
-            outText = rawText.substitute(Generator = generator,
-                                         GeoFile = options.geoFile,
-                                         ScintMaterial = options.scintMaterial,
-                                         OutFileName = outFileName + ".root",
-                                         ExtraDB = extraDB) # Note upper/lower case first letter...
-            outFile = open(outFileName + ".mac", "w")
-            outFile.write(outText)
-            outFile.close()
+                        "/generator/vtx/set " + options.particle + " 0 0 0 " + str(energy) + "\n" + \
+                        "/generator/pos/set 0.0 " + str(position) + " 0.0"
+            outfileName = options.scintMaterial + "_P=" + str(int(position)) + "mm_E=" + str(int(energy * 1000)) + "keV"
+			
+            outText1 = rawText1.substitute(ExtraDB = extraDB,
+                                           GeoFile = options.geoFile,
+                                           ScintMaterial = options.scintMaterial,
+                                           FileName = outfileName + ".root",
+                                           Generator = generator)
+            outFile1 = open(outfileName + ".mac", "w")
+            outFile1.write(outText1)
+            outFile1.close()
 
-            print "Running " + outFileName
-
+            print "Running " + outfileName + ".mac and generating " + outfileName + ".root"
+        
+            # Run the macro on a Batch system
             if options.batch:
-                # run the macro on a batch system 
-                outText = rawScriptText.substitute( Preamble = "\n".join(s for s in batch_params['preamble']),
-                                                    Cwd = os.environ['PWD'].replace("/.", "/"),
-                                                    Macro = outFileName + ".mac",
-                                                    Ratenv = batch_params['ratenv'] )
-                outFile = open(outFileName + ".sh", "w")
-                outFile.write(outText)
-                outFile.close()
-                os.system( batch_params["submit"] + " " + outFileName + ".sh" )
-
+                outText2 = rawText2.substitute(Preamble = "\n".join(s for s in batch_params['preamble']),
+                                               Ratenv = batch_params['ratenv'],
+                                               Cwd = os.environ['PWD'].replace("/.", "/"),
+                                               RunCommand = "rat " + outfileName + ".mac")
+                outFile2 = open(outfileName + ".sh", "w")
+                outFile2.write(outText2)
+                outFile2.close()
+			
+                os.system( batch_params["submit"] + " " + outfileName +".sh" )        
+				
+            # Else run the macro locally on an interactive machine				
             else:
-                # run the macro locally
-                os.system("rat " + outFileName + ".mac")
-                # Now delete the temporary mac file
-                os.remove(outFileName + ".mac")
+                os.system("rat " + outfileName + ".mac")
+                # delete the macro when running is complete
+                os.remove(outfileName + ".mac")
 
 
 import optparse
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage = "usage: %prog [options] target", version="%prog 1.0")
-    parser.add_option("-g", type="string", dest="geoFile", help="Geometry File to use, location must be absolute or relative to target.",
-                      default="geo/snoplus.geo")
-    parser.add_option("-s", type="string", dest="scintMaterial", help="Scintillator material.", default="labppo_scintillator")
-    parser.add_option("-p", type="string", dest="particle", help="Particle type.", default="e-")
-    parser.add_option("-e", type="string", dest="energies", help="Energies (accepts a list)",
-                      action="callback", callback=EnergyLookupUtil.parse_list_arg)
-    parser.add_option("-x", type="string", dest="positions", help="Positions (accepts a list)",
-                      action="callback", callback=EnergyLookupUtil.parse_list_arg)
-    parser.add_option("-b", type="string", dest="batch", help="Run in batch mode" )
-    parser.add_option("-l", type="string", dest="loadDB", help="Load an extra DB directory")
+    parser.add_option("-b", type = "string", dest = "batch", help = "Run the macros in Batch mode")
+    parser.add_option("-g", type = "string", dest = "geoFile", help = "Geometry File to use - location relative to rat/data/, default = geo/snoplus.geo", default = "geo/snoplus.geo")
+    parser.add_option("-l", type = "string", dest = "loadDB", help = "Load an extra .ratdb directory")
+    parser.add_option("-p", type = "string", dest = "particle", help = "Particle type to use (see generator documentation for available particles), default = 'e-'", default = "e-")
+    parser.add_option("-s", type = "string", dest = "scintMaterial", help = "Scintillator Material to use, default = labppo_scintillator", default = "labppo_scintillator")
     (options, args) = parser.parse_args()
-    ProduceRunFiles(options)
+    ProduceRunMacros(options)
+
---
+++ FitCoordination/EnergyLookup/Utilities.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+import ROOT, rat
+# Secondary functions and user-defined Values for the EnergyLookup Coordinator
+# Author P G Jones - 12/09/2011 <p.g.jones@qmul.ac.uk>
+#        K Majumdar - 08/09/2014 - Cleanup of Coordinators for new DS
+
+Positions = [0.0, 2000.0, 4000.0, 5000.0, 5500.0, 5750.0, 5950.0, 6100.0, 6500.0, 7000.0, 7500.0, 8000.0]
+ScintEnergies = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 10.0]    # Use these energies if using a scintillator-filled detector
+WaterEnergies = [3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]    # Use these energies if using a water-filled detector
+
+
+# Return a list of Nhits per Position/Energy combination, arranged as the following for "p" positions and "e" energies:
+# { Nhits(position[0], energy[0]), Nhits(position[0], energy[1]), ... , Nhits(position[0], energy[e-1], Nhits(position[1], energy[0], ... , Nhits(position[p-1], energy[e-1] }
+def NhitsVsPositionEnergy(material):
+
+    # Select which energies to use based on the material in the detector
+    energies = []
+    if material == "lightwater_sno":
+        energies = WaterEnergies
+    else:
+        energies = ScintEnergies
+		
+    nHitsTable = []
+
+    for position in Positions:
+	
+        singlePositionNhitsTable = []
+        for energy in energies:
+		
+            infileName = material + "_P=" + str(int(position)) + "mm_E=" + str(int(energy * 1000)) + "keV"
+
+            Histogram = ROOT.TH1D(infileName, "Nhits", 300, 0.0, 3000.0)
+
+            for ds, run in rat.dsreader(infileName):
+                if ds.GetEVCount() == 0:
+                    continue
+                Histogram.Fill(ds.GetEV(0).GetCalPMTs().GetCount())
+		
+            tolerance = 10
+            lowBin = Histogram.FindFirstBinAbove(0.0)
+            lowBin = lowBin - tolerance
+            highBin = Hitogram.FindLastBinAbove(0.0)
+            highBin = highBin + tolerance
+            if lowBin < 1:
+                lowBin = 1
+            if highBin > Histogram.GetNbinsX():
+                highBin = Histogram.GetNbinsX()
+
+            gaussFit = ROOT.TF1("gaus", "gaus", Histogram.GetBinCenter(lowBin), Histogram.GetBinCenter(highBin))
+            Histogram.Fit(gaussFit, "RQN")
+    
+            singlePositionNhitsTable.append(gaussFit.GetParameter(1))
+			
+            del gaussFit
+            del Histogram
+
+        nHitsTable.append(singlePositionNhitsTable)
+            
+    return nHitsTable
+	
+	
+##### DIAGNOSTIC FUNCTIONS #####
+
+# Return a list of Nhits per Energy/Position combination, arranged as the following for "e" energies and "p" positions:
+# { Nhits(energy[0], position[0]), Nhits(energy[0], position[1]), ... , Nhits(energy[0], position[p-1], Nhits(energy[1], position[0], ... , Nhits(energy[e-1], position[p-1] }
+def NhitsVsEnergyPosition(material):
+
+    # Select which energies to use based on the material in the detector
+    energies = []
+    if material == "lightwater_sno":
+        energies = WaterEnergies
+    else:
+        energies = ScintEnergies
+		
+    nHitsTable = []
+
+    for energy in energies:
+	
+        singleEnergyNhitsTable = []
+        for position in Positions:
+		
+            infileName = material + "_P=" + str(int(position)) + "mm_E=" + str(int(energy * 1000)) + "keV"
+
+            Histogram = ROOT.TH1D(infileName, "Nhits", 300, 0.0, 3000.0)
+
+            for ds, run in rat.dsreader(infileName):
+                if ds.GetEVCount() == 0:
+                    continue
+                Histogram.Fill(ds.GetEV(0).GetCalPMTs().GetCount())
+		
+            tolerance = 10
+            lowBin = Histogram.FindFirstBinAbove(0.0)
+            lowBin = lowBin - tolerance
+            highBin = Hitogram.FindLastBinAbove(0.0)
+            highBin = highBin + tolerance
+            if lowBin < 1:
+                lowBin = 1
+            if highBin > Histogram.GetNbinsX():
+                highBin = Histogram.GetNbinsX()
+
+            gaussFit = ROOT.TF1("gaus", "gaus", Histogram.GetBinCenter(lowBin), Histogram.GetBinCenter(highBin))
+            Histogram.Fit(gaussFit, "RQN")
+    
+            singleEnergyNhitsTable.append(gaussFit.GetParameter(1))
+			
+            del gaussFit
+            del Histogram
+
+        nHitsTable.append(singleEnergyNhitsTable)
+            
+    return nHitsTable
+	
+
+# Return a set of Nhits vs. Position plots (one for each Energy)
+def PlotNHitsPerPosition(material):
+
+    # Select which energies to use based on the material in the detector
+    energies = []
+    if material == "lightwater_sno":
+        energies = WaterEnergies
+    else:
+        energies = ScintEnergies
+
+    nHitsTable = NhitsVsEnergyPosition(material)
+	
+    graphsList = []
+    for energyIndex, energy in enumerate(energies):
+	
+        graph = ROOT.TGraph()
+        for positionIndex in range(0, len(Positions)):
+		
+            graph.SetPoint(positionIndex, Positions[positionIndex], nHitsTable[energyIndex][positionIndex])
+			
+        graph.SetMarkerColor(energyIndex + 2)
+        graphsList.append(graph)
+
+    canvas = ROOT.TCanvas()
+    canvas.Divide(2, len(energies) / 2)
+    for graphIndex, graph in enumerate(graphsList):
+        canvas.cd(graphIndex + 1)
+        graph.Draw("A*")
+        graph.GetYaxis().SetRangeUser(0, 2000)
+
+    raw_input("Press 'Enter' to exit")
+
+
+# Return a set of Nhits vs. Energy plots (one for each Position)
+def PlotNHitsPerEnergy(material):
+
+    # Select which energies to use based on the material in the detector
+    energies = []
+    if material == "lightwater_sno":
+        energies = WaterEnergies
+    else:
+        energies = ScintEnergies
+		
+    legend = ROOT.TLegend(0.1, 0.7, 0.3, 0.9)
+
+    nHitsTable = NhitsVsEnergyPosition(material)
+	
+    graphsList = []
+    graphPoints = []
+    for positionIndex in range(0, len(Positions)):
+	
+        graph = ROOT.TGraph()
+        graph.SetPoint(0, 0.0, 0.0)
+        for energyIndex, energy in enumerate(energies):
+		
+            graph.SetPoint(energyIndex + 1, energy, nHitsTable[energyIndex][positionIndex])
+			
+        graph.SetMarkerColor(positionIndex + 2)
+        graphsList.append(graph)
+		
+    canvas = ROOT.TCanvas()
+    canvas.Divide(2, len(Positions) / 2)
+    for graphIndex, graph in enumerate(graphsList):
+        canvas.cd(graphIndex + 1)
+        graph.Draw("A*")
+        graph.GetYaxis().SetRangeUser(0, 2000)
+
+    raw_input("Press 'Enter' to exit")
+	
+
+ROOT.gROOT.SetStyle("Plain")
+ROOT.gStyle.SetCanvasBorderMode(0)
+ROOT.gStyle.SetPadBorderMode(0)
+ROOT.gStyle.SetPadColor(0)
+ROOT.gStyle.SetCanvasColor(0)
+ROOT.gStyle.SetOptTitle(0)
+ROOT.gStyle.SetOptStat(0)
+
