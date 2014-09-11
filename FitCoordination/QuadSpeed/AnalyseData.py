4ecaef4cceec91347eb7707109d953b819d2ffde
Krishanu Majumdar <majumdar@pplxint8.physics.ox.ac.uk>
2014-09-11 16:05:58 +0100

    Updated QuadSpeed coordinator

--- FitCoordination/QuadSpeed/AnalyseData.py
+++ FitCoordination/QuadSpeed/AnalyseData.py
@@ -1,32 +1,73 @@
 #!/usr/bin/env python
-# Analyses the data and prints the results to screen in ratdb format
-# Author I Coulter - 09/11/2012
-import string
-import ROOT
-import QuadSpeedUtil
+import string. ROOT, Utilities, os
+# Author I T Coulter - 09/11/2012 <icoulter@hep.upenn.edu>
+#        K Majumdar - 11/09/2014 - Cleanup of Coordinators for new DS
 
-def AnalyseData( options ):
-    """ Run over the produced data and output the results in ratdb format."""
-    histograms = QuadSpeedUtil.ProduceAllData()
-    # Fit a straight line
-    endRange = histograms[1].FindLastBinAbove( 1.0 )
-    linearFit = ROOT.TF1( "quad", "[0] + [1]*x", -1000.0, 1000.0 )#histograms[1].GetXaxis().GetBinCenter( endRange ) )
-    histograms[1].Fit( linearFit, "r" )
-    # Print the results
-    print "{\nname: \"QUAD_FIT\","
-    print "index: \"%s\"," % options.index
-    print "valid_begin : [0, 0],\nvalid_end : [0, 0],\n"
-    print "light_speed: %.2fd,  // Effective overall speed of light" % linearFit.GetParameter( 0 )
-    print "num_points: 4000,      // Max points generated in quadcloud"
-    print "limit_points: 20000,   // Number of attempts to generate points"
-    print "calc_time: 1,       // Whether to calcuate the reconstructed time"
-    print "calc_errors: 1,     // Whether to compute errors on time & position"
+
+def AnalyseRootFiles(options):
+
+    # Load any parameters for running the macros on a Batch system
+    batch_params = None
+    if options.batch:
+        batch_params = {}
+    execfile(options.batch, {}, batch_params)
+	
+    # Load the batch submission script template
+    inFile = open("Template_Batch.sh", "r")
+    rawText = string.Template(inFile.read())
+    inFile.close()
+		
+    # Run the analysis on a Batch system
+    if options.batch:
+        outText = rawText.substitute(Preamble = "\n".join(s for s in batch_params['preamble']),
+                                     Ratenv = batch_params['ratenv'],
+                                     Cwd = os.environ['PWD'].replace("/.", "/"),
+                                     RunCommand = "python -c 'import AnalyseData; AnalyseData.AnalysisFunction(\"" + options.index + "\")'")
+        outFile = open("AnalyseData.sh", "w")
+        outFile.write(outText)
+        outFile.close()
+		
+        os.system(batch_params["submit"] + " AnalyseData.sh")
+		
+    # Else run the macro locally on an interactive machine				
+    else:
+        os.system("python -c 'import AnalyseData; AnalyseData.AnalysisFunction(\"" + options.index + "\")'")
+		
+
+# returns the effective speed of light and other quad-fitter parameters
+def AnalysisFunction(index):
+
+    histograms = Utilities.ProducePlots()
+
+    # Fit a straight line to the 1D Radial Bias vs. Effective Speed plot
+    endRange = histograms[1].FindLastBinAbove(1.0)
+    linearFit = ROOT.TF1("linearFit", "pol1", -1000.0, 1000.0)
+    histograms[1].Fit(linearFit, "R")
+    
+    # Print the final results
+    print "\n"
+    print "Please place the text below into the database file: FIT_QUAD.ratdb located in rat/data, replacing any existing entry with the same index."
+    print "\n"
+    print "{"
+    print "name: \"QUAD_FIT\","
+    print "index: \"" + index + "\","
+    print "valid_begin: [0, 0],"
+    print "valid_end: [0, 0],"
+    print "\n",
+    print "light_speed: " + str(linearFit.GetParameter(0)) + "d,    // Effective overall speed of light"
+    print "num_points: 4000,    // Maximum number of points generated in quad-cloud"
+    print "limit_points: 20000, // Number of attempts to generate points"
+    print "calc_time: 1,        // Whether to calculate the reconstructed time"
+    print "calc_errors: 1,      // Whether to compute errors on time and position"
     print "}"
+    print "\n"
+
 
 import optparse
 if __name__ == '__main__':
     parser = optparse.OptionParser( usage = "usage: %prog [options] target", version="%prog 1.0" )
-    parser.add_option( "-i", type="string", dest="index", help="RATDB index to place result.", default="" )
+    parser.add_option("-b", type = "string", dest = "batch", help = "Run the macros in Batch mode")
+    parser.add_option("-i", type = "string", dest = "index", help = "RATDB index to place result.", default = "")
     (options, args) = parser.parse_args()
-    AnalyseData( options )
+    AnalyseRootFiles(options)
     
--- FitCoordination/QuadSpeed/README.md
+++ FitCoordination/QuadSpeed/README.md
@@ -1,11 +1,54 @@
 # QuadSpeed Coordinator
-To run use the fitcoordinate script one directory up from this. Then do:
+This folder contains the files needed to coordinate the QuadSpeed fitter.  
+There are two methods for running the coordinator:
 
-    python fitcoordinate QuadSpeed
+-------------------------
 
-Normal fit coordination options apply.
+1) standard method, which is the same as other coordinators:
+- navigate to one directory up from this, and then do:
 
-## Diagnostic scripts
-This folder contains a diagnostic plotting script, to plot the EffectiveSpeed against RadialBias. To run, first coordinate then for the EffectiveSpeed v RadialBias plot:
+    python fitcoordinate [options] QuadSpeed
 
-    python DiagnosticPlotFiles.py
+The following fit coordination options apply:
+- [-d]: A location in which to run the scripts, e.g. on a data disk (default = [empty])
+- [-g]: Geometry File to use ... location relative to rat/data/ (default = geo/snoplus.geo)
+- [-i]: RATDB index to place result (default = [empty])
+- [-l]: Load an extra .ratdb directory
+- [-p]: Particle type to use ... see generator documentation for available particles (default = 'e-')
+- [-s]: Scintillator Material to use (default = labppo_scintillator)
+
+This method first runs the ProduceData.py script, which generates a set of rootfiles (one for each effective speed) of 500 events each.  
+Once this is done, the AnalyseData.py script automatically begins.  
+The coordination results are written to screen - there will be a complete RATDB entry that should be placed in the FIT_QUAD.ratdb located in rat/data, replacing any existing entry with the same index.  
+
+Note: this coordinator can be used to find the effective speed of an unknown material, by taking "labppo_scintillator" as a starting point and proceeding from there.  
+If this is desired, please follow the instructions given in the comments of the Template_Macro.mac file.  
+
+-------------------------
+
+2) batch method, which needs to be invoked differently from the standard method:
+- in this folder, run the command:
+
+    python ProduceData.py [options]
+
+The options for this script are: [-g], [-l], [-p] and [-s] as specified above, as well as:
+- [-b]: Batch configuration file ... absolute location
+
+There already exists a basic "batch.config" file in the "FitCoordination" folder.  However, users may specify their own configuration using that file as a template, and then provide the filename of their new configuration file here.  
+
+- once the production script is complete, the analysis script will NOT begin automatically - it must be run by the user.  To do this, while still in this folder, run the command:
+
+    python AnalyseData.py [options]
+
+The only applicable options for this script are [-b] and [-i] as described above.  
+The coordination results are written to the Batch logfile - there will be a complete RATDB entry that should be placed in the FIT_QUAD.ratdb located in rat/data, replacing any existing entry with the same index.  
+
+-------------------------
+
+The Utilities script contains the following function that can be used to get extra information about the Radial Bias vs. Effective Speed relation:  
+- DrawPlot(): return one 2D scatter plot - Radial Bias vs. Effective Speed  
+
+To run this function, first run the ProduceData script as described above, and then do the following:
+
+    python -c 'import Utilities.py; Utilities.DrawPlot()'
+
