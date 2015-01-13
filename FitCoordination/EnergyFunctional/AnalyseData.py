5321d969cdabfc2e2b8cd1348b34f4cd6f866890
Krishanu Majumdar <majumdar@pplxint8.physics.ox.ac.uk>
2015-01-13 11:00:03 +0000

    EnergyFunctional Coordinator now works for both internal and external events

--- FitCoordination/EnergyFunctional/AnalyseData.py
+++ FitCoordination/EnergyFunctional/AnalyseData.py
@@ -1,6 +1,6 @@
 #!usr/bin/env python
 import string, Utilities, os
-# Author K Majumdar - 08/09/2014 <Krishanu.Majumdar@physics.ox.ac.uk>
+# Author K Majumdar - 11/01/2015 <Krishanu.Majumdar@physics.ox.ac.uk>
 
 
 def AnalyseRootFiles(options):
@@ -21,28 +21,29 @@ def AnalyseRootFiles(options):
         outText = rawText.substitute(Preamble = "\n".join(s for s in batch_params['preamble']),
                                      Ratenv = batch_params['ratenv'],
                                      Cwd = os.environ['PWD'].replace("/.", "/"),
-                                     RunCommand = "python -c 'import AnalyseData; AnalyseData.AnalysisFunction(\"" + options.index + "\")'")
+                                     RunCommand = "python -c 'import AnalyseData; AnalyseData.AnalysisFunction(\"" + options.index + "\", \"" + options.scintMaterial + "\")'")
         outFile = open("AnalyseData.sh", "w")
         outFile.write(outText)
         outFile.close()
 		
         os.system(batch_params["submit"] + " AnalyseData.sh")
 		
-    # Else run the macro locally on an interactive machine				
+    # Else run the analysis locally on an interactive machine				
     else:
-        os.system("python -c 'import AnalyseData; AnalyseData.AnalysisFunction(\"" + options.index + "\")'")
+        os.system("python -c 'import AnalyseData; AnalyseData.AnalysisFunction(\"" + options.index + "\", \"" + options.scintMaterial + "\")'")
 		
 
 # returns the parameters for the Functional Form of Energy Reconstruction in the form of a complete RATDB entry
-def AnalysisFunction(index):
+def AnalysisFunction(index, material):
     
-    Utilities.PlotsForEnergyCoeffs()
-    energyCoefficients = Utilities.ExtractEnergyCoeffs()
+    Utilities.PlotsForEnergyCoeffs(0, material)
+    Utilities.PlotsForEnergyCoeffs(1, material)
+    energyCoefficients = Utilities.ExtractEnergyCoeffs(material)
 
-    Utilities.PlotsForRadiusCoeffs(energyCoefficients)
+    Utilities.PlotsForRadiusCoeffs(energyCoefficients, material)
     radiusCoefficients = Utilities.ExtractRadiusCoeffs()
 	
-    Utilities.PlotsForZCoeffs(energyCoefficients, radiusCoefficients)
+    Utilities.PlotsForZCoeffs(energyCoefficients, radiusCoefficients, material)
     zCoefficients = Utilities.ExtractZCoeffs()
 	
 	##############################
@@ -61,37 +62,45 @@ def AnalysisFunction(index):
     diagFile.write("\n")
 
     diagFile.write("energyCoeffs: ["),
-    energyCoeffStrings = []
     for coeff in energyCoefficients:
-        coeffString = str(coeff) + ", "
-        diagFile.write(coeffString)
+        diagFile.write(str(coeff) + ", ")
     diagFile.write("], \n")
 
     radiusCoeffStrings = []
     for coeff in radiusCoefficients:
-        coeffString = str(coeff) + ", "
-        radiusCoeffStrings.append(coeffString)
+        radiusCoeffStrings.append(str(coeff) + ", ")
 
-    diagFile.write("radiusCoeffsLow: ["),
-    for coeff in range(0, Utilities.numberOfRadiusCoeffs_Low):
+    diagFile.write("radiusCoeffsInternalLow: ["),
+    for coeff in range(0, Utilities.numberOfRadiusCoeffs_Internal_Low):
         diagFile.write(radiusCoeffStrings[coeff])
     diagFile.write("], \n")
-    diagFile.write("midRadiusCut: " + str(Utilities.radiusFitRange_Mid[0]) + ", \n")
-    diagFile.write("radiusCoeffsMid: ["),
-    for coeff in range(Utilities.numberOfRadiusCoeffs_Low, (Utilities.numberOfRadiusCoeffs_Low + Utilities.numberOfRadiusCoeffs_Mid)):
+    diagFile.write("internalMidRadiusCut: " + str(Utilities.radiusFitRange_Internal_Mid[0]) + ", \n")
+    diagFile.write("radiusCoeffsInternalMid: ["),
+    for coeff in range(Utilities.numberOfRadiusCoeffs_Internal_Low, (Utilities.numberOfRadiusCoeffs_Internal_Low + Utilities.numberOfRadiusCoeffs_Internal_Mid)):
         diagFile.write(radiusCoeffStrings[coeff])
     diagFile.write("], \n")	
-    diagFile.write("highRadiusCut: " + str(Utilities.radiusFitRange_High[0]) + ", \n")
-    diagFile.write("radiusCoeffsHigh: ["),
-    for coeff in range((Utilities.numberOfRadiusCoeffs_Low + Utilities.numberOfRadiusCoeffs_Mid), len(radiusCoeffStrings)):
+    diagFile.write("internalHighRadiusCut: " + str(Utilities.radiusFitRange_Internal_High[0]) + ", \n")
+    diagFile.write("radiusCoeffsInternalHigh: ["),
+    for coeff in range((Utilities.numberOfRadiusCoeffs_Internal_Low + Utilities.numberOfRadiusCoeffs_Internal_Mid), (Utilities.numberOfRadiusCoeffs_Internal_Low + Utilities.numberOfRadiusCoeffs_Internal_Mid + Utilities.numberOfRadiusCoeffs_Internal_High)):
         diagFile.write(radiusCoeffStrings[coeff])
-    diagFile.write("], \n")	
-
+    diagFile.write("], \n")
+    diagFile.write("externalLowRadiusCut: " + str(Utilities.radiusFitRange_External_Low[0]) + ", \n")
+    diagFile.write("radiusCoeffsExternalLow: ["),
+    for coeff in range((Utilities.numberOfRadiusCoeffs_Internal_Low + Utilities.numberOfRadiusCoeffs_Internal_Mid + Utilities.numberOfRadiusCoeffs_Internal_High), (Utilities.numberOfRadiusCoeffs_Internal_Low + Utilities.numberOfRadiusCoeffs_Internal_Mid + Utilities.numberOfRadiusCoeffs_Internal_High + Utilities.numberOfRadiusCoeffs_External_Low)):
+        diagFile.write(radiusCoeffStrings[coeff])
+    diagFile.write("], \n")		
+    diagFile.write("externalHighRadiusCut: " + str(Utilities.radiusFitRange_External_High[0]) + ", \n")
+    diagFile.write("radiusCoeffsExternalHigh: ["),
+    for coeff in range((Utilities.numberOfRadiusCoeffs_Internal_Low + Utilities.numberOfRadiusCoeffs_Internal_Mid + Utilities.numberOfRadiusCoeffs_Internal_High + Utilities.numberOfRadiusCoeffs_External_Low), len(radiusCoeffStrings)):
+        diagFile.write(radiusCoeffStrings[coeff])
+    diagFile.write("], \n")
+	
+    diagFile.write("lowZLimit: " + str(Utilities.zFitRange[0]) + ", \n")
+    diagFile.write("highZLimit: " + str(Utilities.zFitRange[1]) + ", \n")
     diagFile.write("zCoeffs: ["),
     zCoeffStrings = []
     for coeff in zCoefficients:
-        coeffString = str(coeff) + ", "
-        diagFile.write(coeffString)
+        diagFile.write(str(coeff) + ", ")
     diagFile.write("], \n")
 	
     diagFile.write("} \n")
@@ -106,6 +115,7 @@ if __name__ == '__main__':
     parser = optparse.OptionParser(usage = "usage: %prog [options] target", version = "%prog 1.0")
     parser.add_option("-b", type = "string", dest = "batch", help = "Run the analysis in Batch mode")
     parser.add_option("-i", type = "string", dest = "index", help = "RATDB index to place result.", default = "")
+    parser.add_option("-s", type = "string", dest = "scintMaterial", help = "Scintillator Material to use, default = labppo_scintillator", default = "labppo_scintillator")
     (options, args) = parser.parse_args()
     AnalyseRootFiles(options)
 
--- FitCoordination/EnergyFunctional/ProduceData.py
+++ FitCoordination/EnergyFunctional/ProduceData.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os, sys, string, Utilities
-# Author K Majumdar - 04/09/2014 <Krishanu.Majumdar@physics.ox.ac.uk>
+# Author K Majumdar - 09/01/2015 <Krishanu.Majumdar@physics.ox.ac.uk>
 
 
 def ProduceRunMacros(options):
@@ -26,22 +26,53 @@ def ProduceRunMacros(options):
     rawText2 = string.Template(inFile2.read())
     inFile2.close()
 	
-    # Need 5000 events per Parameter per Energy ... go quicker by doing 5 files of 1000 events each
-    for energy in Utilities.energies:
+    # Loop over the fit parameters to be calculated (each one needs a separate set of events)
+    for i in range(len(Utilities.parameters)):
+	
+        # Check what kind of parameter this loop is for, based on the parameter index "i"
+        # If "internal energyCoeffs" (i = 0), check what material has been set and load the correct energies
+		# If "external energyCoeffs" (i = 1), set material to always be water, and load the correct energies
+		# If "radialCoeffs" (i = 2), load the correct energies
+		# If "zCoeffs" (i = 3), load the correct energies
+        if (i == 0):
+            material = options.scintMaterial
 
-        for i in range(len(Utilities.parameters)):
+            if (material == "lightwater_sno"):
+                energyList = list(Utilities.energies_energyCoeffs_water)
+            else:
+                energyList = list(Utilities.energies_energyCoeffs_scintillator)
+        elif (i == 1):
+            material = "lightwater_sno"
+            energyList = list(Utilities.energies_energyCoeffs_water)
+        elif (i == 2):
+            material = options.scintMaterial
+            energyList = list(Utilities.energies_radiusCoeffs)
+        else:
+            material = options.scintMaterial
+            energyList = list(Utilities.energies_zCoeffs)
+            
+        # For each parameter, simulate the correct number of events at each energy (5 files per energy per parameter)
+        for energyIndex, energy in enumerate(energyList):
 
             for p in range (1, 6):
 
                 outfileName = "electrons_" + Utilities.parameters[i] + "_" + str(int(energy * 1000)) + "keV_" + Utilities.positionTypes[i] + "_part" + str(p)
+				
+                # For the z-function, need to fill 3 separate volumes (inner_av, av and water), so need 3 separate position generator lines
+                positionLine = ""
+                if (i != 3):
+                    positionLine += "/generator/pos/set " + Utilities.positionArgs[i] + "\n"
+                else:
+                    for positionIndex in range(3, len(Utilities.positionArgs)):
+                        positionLine += "/generator/pos/set " + Utilities.positionArgs[positionIndex] + "\n"			
 
                 outText1 = rawText1.substitute(ExtraDB = extraDB,
                                                GeoFile = options.geoFile,
-                                               ScintMaterial = options.scintMaterial,
-											   FileName = outfileName + ".root",
+                                               ScintMaterial = material,
+                                               FileName = outfileName + ".root",
                                                PositionType = Utilities.positionTypes[i],
                                                Energy = energy,
-                                               PositionArg = Utilities.positionArgs[i])
+                                               PositionArg = positionLine)
                 outFile1 = open(outfileName + ".mac", "w")
                 outFile1.write(outText1)
                 outFile1.close()
