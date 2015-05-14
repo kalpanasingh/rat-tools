9c622d2786337910ecc5ff5cbd239f563e367d6f
David Auty <auty@ualberta.ca>
2015-05-14 10:08:36 -0600

    Added a file to delete files on grid

--- GridTools/utilities.py
+++ GridTools/utilities.py
@@ -13,6 +13,8 @@
 #
 #####################
 
+import numpy
+import os
 import zlib
 
 def get_adler32(filename):
@@ -30,3 +32,39 @@ def get_adler32(filename):
             val += 2**32
     f.close()
     return hex(val)[2:10].zfill(8).lower()
+
+def readin_file(textfile):
+    # read in the text file and split up the input into the correct
+    # varibles
+    directory = []
+    file_path = []
+    filename = []
+    words = []
+    with open(textfile, 'r') as f:
+        data = f.readlines()
+        for line in data:
+            line = line.translate(None, '\n\t ')
+            words.append(line.split(','))
+        words_array = numpy.array(words)
+        if words_array.shape[1] != 3:
+            print "Not correct number of inputs"
+            print "should be:"
+            print repr("<base directory>,<remote path>,<local file>")
+            print "you have used inputs"
+            print words_array.shape[1]
+            sys.exit()
+            return 0
+        directory = words_array[:, 0]
+        file_path = words_array[:, 1]
+        filename = words_array[:, 2]
+    return directory, file_path, filename
+
+def delete_file(lfc_path):
+    input_string = 'lcg-del -a %s' % (lfc_path)
+    os.system(input_string)
+    return
+
+def delete_folder(lfc_path):
+    input_string = 'lcg-del -d %s' % (lfc_path)
+    os.system(input_string)
+    return
