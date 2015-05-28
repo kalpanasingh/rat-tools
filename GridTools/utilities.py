849eec6954f892b7c0828db5818185702e8b92d7
Matt Mottram <mjmottram@gmail.com>
2015-05-28 17:20:30 +0100

    Added Orca log grabber

--- GridTools/utilities.py
+++ GridTools/utilities.py
@@ -30,3 +30,22 @@ def get_adler32(filename):
             val += 2**32
     f.close()
     return hex(val)[2:10].zfill(8).lower()
+
+
+def read_grabber_file(filename):
+    copy_type = None
+    files = []
+    sizes = []
+    guids = []
+    adlers = []
+    with open(filename, "r") as fin:
+        copy_type = fin.readline().strip()
+        if copy_type!="GUID" and copy_type!="SURL":
+            raise Exception("Unknown copy type")
+        for line in fin.readlines():
+            f, s, g, a = line.split()
+            files.append(f.strip())
+            sizes.append(int(s.strip()))
+            guids.append(g.strip())
+            adlers.append(a.strip())
+    return copy_type, files, sizes, guids, adlers
