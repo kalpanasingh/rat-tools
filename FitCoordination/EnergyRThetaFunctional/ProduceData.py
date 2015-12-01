db0fc3183da0be56ec8e65fe84992fdd88f9bf2f
Matt Mottram <mjmottram@gmail.com>
2015-12-01 11:38:02 +0000

    added EnergyRThetaFunctional

---
+++ FitCoordination/EnergyRThetaFunctional/ProduceData.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# Author M Mottram <m.mottram@qmul.ac.uk>
+import os
+import sys
+import string
+import math
+import Utilities
+
+def submit_job(options, generator, outfileName):
+
+    # Load any parameters for running the macros on a Batch system
+    batch_params = None
+    if options.batch:
+        batch_params = {}
+        execfile(options.batch, {}, batch_params)
+    
+    # Load any extra .ratdb files
+    extraDB = ""
+    if options.loadDB:
+        extraDB = "/rat/db/load " + options.loadDB
+    
+    # Load the basic macro template
+    inFile1 = open("Template_Macro.mac", "r")
+    rawText1 = string.Template(inFile1.read())
+    inFile1.close()
+        
+    # Load the batch submission script template
+    inFile2 = open("Template_Batch.sh", "r")
+    rawText2 = string.Template(inFile2.read())
+    inFile2.close()
+
+    outText1 = rawText1.substitute(ExtraDB = extraDB,
+                                   GeoFile = options.geoFile,
+                                   ScintMaterial = options.scintMaterial,
+                                   FileName = outfileName + ".root",
+                                   Generator = generator)
+    outFile1 = open(outfileName + ".mac", "w")
+    outFile1.write(outText1)
+    outFile1.close()
+
+    print "Running " + outfileName + ".mac and generating " + outfileName + ".root"
+    
+    # Run the macro on a Batch system
+    if options.batch:
+        
+        outText2 = rawText2.substitute(Preamble = "\n".join(s for s in batch_params['preamble']),
+                                       Ratenv = batch_params['ratenv'],
+                                       Cwd = os.environ['PWD'].replace("/.", "/"),
+                                       RunCommand = "rat " + outfileName + ".mac")
+        outFile2 = open(outfileName + ".sh", "w")
+        outFile2.write(outText2)
+        outFile2.close()
+        
+        os.system( batch_params["submit"] + " " + outfileName +".sh" )        
+    			
+    # Else run the macro locally on an interactive machine				
+    else:
+        os.system("rat " + outfileName + ".mac")
+        # delete the macro when running is complete
+        os.remove(outfileName + ".mac")
+
+
+
+def ProduceRunMacros(options):
+    
+    # Generator energies at the centre
+    for energy in Utilities.CentralEnergies:
+
+        generator = "/generator/add combo gun:point\n" + \
+            "/generator/vtx/set " + options.particle + " 0 0 0 " + str(energy) + "\n" + \
+            "/generator/pos/set 0 0 0" # Generate at the centre of the detector
+        outfileName = Utilities.CentralFilename(options.scintMaterial, energy)
+        
+        submit_job(options, generator, outfileName)
+        
+
+    # Generate positions on a plane
+    for i in range(1, 51):
+
+        generator = "/generator/add combo gun:plane\n" + \
+            "/generator/vtx/set " + options.particle + " 0 0 0 " + str(Utilities.LookupEnergy) + "\n" + \
+            "/generator/pos/set 0 0 0 0 1 0 6000" # Generate on a plane in Y = 0
+        outfileName = Utilities.PlaneFilename(options.scintMaterial, i)
+        
+        submit_job(options, generator, outfileName)
+
+
+
+import optparse
+if __name__ == '__main__':
+    parser = optparse.OptionParser(usage = "usage: %prog [options] target", version = "%prog 1.0")
+    parser.add_option("-b", type = "string", dest = "batch", help = "Run the macros in Batch mode")
+    parser.add_option("-g", type = "string", dest = "geoFile", help = "Geometry File to use - location relative to rat/data/, default = geo/snoplus.geo", default = "geo/snoplus.geo")
+    parser.add_option("-l", type = "string", dest = "loadDB", help = "Load an extra .ratdb directory")
+    parser.add_option("-p", type = "string", dest = "particle", help = "Particle type to use (see generator documentation for available particles), default = 'e-'", default = "e-")
+    parser.add_option("-s", type = "string", dest = "scintMaterial", help = "Scintillator Material to use, default = labppo_scintillator", default = "labppo_scintillator")
+    (options, args) = parser.parse_args()
+    ProduceRunMacros(options)
+
---
+++ FitCoordination/EnergyRThetaFunctional/README.md
@@ -0,0 +1,57 @@
+# EnergyLookup Coordinator
+This folder contains the files needed to coordinate the EnergyLookup fitter.  
+There are two methods for running the coordinator:
+
+-------------------------
+
+1) standard method, which is the same as other coordinators:
+- navigate to one directory up from this, and then do:
+
+    python fitcoordinate [options] EnergyLookup
+
+The following fit coordination options apply:
+- [-d]: A location in which to run the scripts, e.g. on a data disk (default = [empty])
+- [-g]: Geometry File to use ... location relative to rat/data/ (default = geo/snoplus.geo)
+- [-i]: RATDB index to place result (default = [empty])
+- [-l]: Load an extra .ratdb directory
+- [-p]: Particle type to use ... see generator documentation for available particles (default = 'e-')
+- [-s]: Scintillator Material to use (default = labppo_scintillator)
+
+This method first runs the ProduceData.py script, which generates a set of rootfiles (one for each energy/position combination) of 500 events each.  Each one takes roughly 1 to 1.5 hours to complete, depending mainly on the scintillator material being used in the simulation.  
+Once this is done, the AnalyseData.py script automatically begins - this takes between 1-2 hours to complete.  
+The coordination results are written to screen - there will be a complete RATDB entry that should be placed in the FIT_ENERGY_LOOKUP.ratdb located in rat/data, replacing any existing entry with the same index.  
+
+** NOTE: the standard method of coordination takes upwards of 3 days to complete, since the rootfiles are generated one after the other, so it is strongly advised that the user perform the coordination on a batch system using the 2nd method below.
+
+-------------------------
+
+2) batch method, which needs to be invoked differently from the standard method:
+- copy this entire folder to a location with around 5GB of free disk-space
+- navigate into this new folder, and run the command:
+
+    python ProduceData.py [options]
+
+The options for this script are: [-g], [-l], [-p] and [-s] as specified above, as well as:
+- [-b]: Batch configuration file ... absolute location
+
+There already exists a basic "batch.config" file in the "FitCoordination" folder.  However, users may specify their own configuration using that file as a template, and then provide the filename of their new configuration file here.  
+
+- once the production script is complete (i.e. all rootfiles have been generated), the analysis script will NOT begin automatically - it must be run by the user.  To do this, while still in this folder, run the command:
+
+    python AnalyseData.py [options]
+
+The only applicable options for this script are [-b], [-i] and [-s] as described above, and it takes roughly 30 minutes to complete.  
+The coordination results are written to the Batch logfile - there will be a complete RATDB entry that should be placed in the FIT_ENERGY_LOOKUP.ratdb located in rat/data, replacing any existing entry with the same index.  
+
+-------------------------
+
+The Utilities script contains the following functions that can be used to get extra information about the Nhits vs. Energy vs. Position relations:  
+- PlotNHitsPerPosition(material): return a set of Nhits vs. Position plots (one for each Energy)  
+- PlotNHitsPerEnergy(material): return a set of Nhits vs. Energy plots (one for each Position)  
+
+To run these functions, first run the ProduceData script as described above, and then do the following:
+
+    python -c 'import Utilities.py; Utilities.[name of function](material)'
+
+where the [name of function] is as noted above, and the "material" argument is the same as that used to produce the data.
+
